838477b4a062403c3cec868e7fd72418b2858dd9
tbrownex <tbrownex@gmail.com>
2019-09-20 16:34:28 +0000

    added 'stratify' to splitData; fixed MinMax for normalize

--- normalizeData.py
+++ normalizeData.py
@@ -1,4 +1,4 @@
-from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
 import pandas as pd
 
 def normalize(dataDict, method):
@@ -7,41 +7,21 @@ def normalize(dataDict, method):
     Normalize Train using "fit" which will set the Mean and StdDev. Then "transform" all the data using
     those values from "fit"
     '''
-    assert method in ['MinMaxA','MinMaxB', 'Std', 'tanh'],  "Invalid scaling method"
+    assert method in ['MinMax', 'Std'],  "Invalid scaling method"
     
-    if method =='MinMaxA':
+    cols = dataDict["trainX"].columns
+    if method =='MinMax':
         scaler = MinMaxScaler()
-        data   = pd.DataFrame(min_max_scaler.fit_transform(dataDict))
-        data.columns   = sv_cols
-        scale_range    = min_max_scaler.data_range_[-1]
-        scale_min      = min_max_scaler.data_min_[-1]
-        return data, scale_range, scale_min
-    elif method =='MinMaxB':
-        arr        = dataDict.values
-        width      = np.ptp(arr, axis=0)
-        scale_min  = np.min(arr, axis=0)
-        norm       = (2 * (arr - scale_min) / width) -1
-        data       = pd.DataFrame(norm, columns=sv_cols)
-        return data, width[-1], scale_min[-1]
-    elif method == 'Std':
-        # StandardScaler returns a numpy array so need to recreate the DF
-        cols = dataDict["trainX"].columns
-        scaler = StandardScaler()
-        scaler.fit(dataDict["trainX"])
-        arr = scaler.transform(dataDict["trainX"])
-        dataDict["trainX"] = pd.DataFrame(arr, columns=cols)
-        if dataDict["valX"] is not None:
-            arr = scaler.transform(dataDict["valX"])
-            dataDict["valX"] = pd.DataFrame(arr, columns=cols)
-        if dataDict["testX"] is not None:
-            arr = scaler.transform(dataDict["testX"])
-            dataDict["testX"] = pd.DataFrame(arr, columns=cols)
-        return dataDict
     else:
-        arr = dataDict.values
-        std = np.std(arr,axis=0)
-        avg = np.mean(arr,axis=0)
-        Z = (arr - avg) / std
-        norm = 0.5 * (np.tanh( Z * .01 ) + 1)
-        data = pd.DataFrame(norm, columns=sv_cols)
-        return data, std[-1], avg[-1]
+        scaler = StandardScaler()
+        
+    scaler.fit(dataDict["trainX"])
+    arr = scaler.transform(dataDict["trainX"])
+    dataDict["trainX"] = pd.DataFrame(arr, columns=cols)
+    if "valX" in dataDict.keys():
+        arr = scaler.transform(dataDict["valX"])
+        dataDict["valX"] = pd.DataFrame(arr, columns=cols)
+    if "testX" in dataDict.keys():
+        arr = scaler.transform(dataDict["testX"])
+        dataDict["testX"] = pd.DataFrame(arr, columns=cols)
+    return dataDict
--- splitData.py
+++ splitData.py
@@ -1,15 +1,16 @@
 from sklearn.model_selection import train_test_split
 import pandas as pd
 
-def splitData(data, config, seed=None):
+def splitData(data, config, stratify=None, seed=None):
     '''
     Incoming dataframe will be split into Train/Val/Test and returned as a dictionary
+    If "stratify' is set, then the splits will have balanced classes (for classification)
     '''
     d = {}
     svCols = data.columns
     
     if config["testPct"] > 0:
-        train, test = train_test_split(data, test_size=config["testPct"], random_state =seed)
+        train, test = train_test_split(data, test_size=config["testPct"], stratify=data[stratify], random_state =seed)
         train.columns = svCols
         test.columns  = svCols
         d["train"] = train
@@ -18,7 +19,7 @@ def splitData(data, config, seed=None):
         d["train"] = data 
         
     if config["valPct"] > 0:
-        train, val = train_test_split(d["train"], test_size=config["valPct"], random_state =seed)
+        train, val = train_test_split(d["train"], test_size=config["valPct"], stratify=d["train"][stratify], random_state =seed)
         train.columns = svCols
         val.columns  = svCols
         d["train"] = train
